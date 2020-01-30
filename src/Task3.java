855f256120db6d0a68432be1745e83e150920cd0
DanDen04 <donil.denisenko.04@mail.ru>
2020-01-30 18:38:04 +0300

    [ADD] Task 2 var 1 switch Massive

--- src/Task3.java
+++ src/Task3.java
@@ -6,13 +6,27 @@ public class Task3 {
             int result = 0;
 
             switch(variant) {
+//                case 1 :
+//                    int[] array = {1, 2, 3, 4, 5, 6, 9, 10};
+//
+//                    for (int i = 0; i < array.length; i++) {
+//                        if (array [i] % 3 ==0) result += array[i];
+//                    }
+//                 break;
                 case 1 :
-                    int[] array = {1, 2, 3, 4, 5, 6, 9, 10};
+
+                    Scanner in = new Scanner(System.in);
+
+                    int[] array = new int[5];
 
                     for (int i = 0; i < array.length; i++) {
-                        if (array [i] % 3 ==0) result += array[i];
+                        array[i] = in.nextInt();
                     }
-                 break;
+
+                    for (int i : array) {
+                        if (i % 3 ==0) result += i;
+                    }
+                break;
             }
         System.out.println("Ответ " + result);
     }
