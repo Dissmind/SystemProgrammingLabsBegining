cb10141b2f1a3e5a0144790929d87f19e61b124f
DanDen04 <donil.denisenko.04@mail.ru>
2020-02-02 01:01:40 +0300

    [ADD] Task 2 var 2 switch Massive

--- src/Task3.java
+++ src/Task3.java
@@ -4,54 +4,34 @@ public class Task3 {
     public static void start(int variant) {
         Scanner in = new Scanner(System.in);
         System.out.println("Сколько элементов в массиве?");
+        int a = in.nextInt();
+        int[] array = new int[a];
 
-            int result = 0;
+        if (a >= 21 | a <= 1 ) return;
+
+        System.out.println("Ввод >>> ");
+
+        int result = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = in.nextInt();
+        }
 
             switch(variant) {
-//                case 1 :
-//                    int[] array = {1, 2, 3, 4, 5, 6, 9, 10};
-//
-//                    for (int i = 0; i < array.length; i++) {
-//                        if (array [i] % 3 ==0) result += array[i];
-//                    }
-//                 break;
                 case 1 :
-                    int a = in.nextInt();
-
-                    int[] array = new int[a];
-
-                    if (a > 20) break;
-
-                    System.out.println("Ввод >>> ");
-
-                    for (int i = 0; i < array.length; i++) {
-                        array[i] = in.nextInt();
-                    }
-
+                    result = 0;
                     for (int i : array) {
                         if (i % 3 == 0) result += i;
                     }
                 break;
 
                 case 2 :
-                    int b = in.nextInt();
-
-                    int[] array2 = new int[b];
-
-                    if (b > 20) break;
-
-                    System.out.println("Ввод >>> ");
-
-                    for (int i = 0; i < array2.length; i++) {
-                        array2[i] = in.nextInt();
-                    }
-
-                    for (int i : array2) {
+                    result = 1;
+                    for (double i : array) {
                         if (i % 5 != 0) result *= i;
                     }
                 break;
             }
-            if (result == 0) System.out.println("Error 147");
-            else System.out.println("Ответ >>> " + result);
+            System.out.println("Ответ >>> " + result);
     }
 }
