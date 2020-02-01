7c1a9d5d305664fa80e42fd22c9ada994e7f2f21
DanDen04 <donil.denisenko.04@mail.ru>
2020-02-02 01:35:20 +0300

    [ADD] Task 3 var 2 switch Massive

--- src/Task3.java
+++ src/Task3.java
@@ -4,10 +4,22 @@ public class Task3 {
     public static void start(int variant) {
         Scanner in = new Scanner(System.in);
         System.out.println("Сколько элементов в массиве?");
+        int a = in.nextInt();
+
+        int[] array = new int[a];
+
+        if (a >= 21 | a <= 0) return;
+
+        System.out.println("Ввод >>> ");
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = in.nextInt();
+        }
 
             int result = 0;
 
             switch(variant) {
+
 //                case 1 :
 //                    int[] array = {1, 2, 3, 4, 5, 6, 9, 10};
 //
@@ -19,18 +31,6 @@ public class Task3 {
                     // TODO: Принцип DRY (don't repeat yourself, с буржуйского - неповторяйся)
                     // TODO: Почему бы не сделать отдельный метод
                     // TODO: Тем более у тебя всегда эта часть должна быть в начале, изменяется лишь алгоритм (посмотри как ты это решил в Task2)
-                    int a = in.nextInt();
-
-                    int[] array = new int[a];
-
-                    if (a > 20) break;
-
-                    System.out.println("Ввод >>> ");
-
-                    for (int i = 0; i < array.length; i++) {
-                        array[i] = in.nextInt();
-                    }
-
                     for (int i : array) {
                         if (i % 3 == 0) result += i;
                     }
@@ -38,25 +38,13 @@ public class Task3 {
 
                 case 2 :
                     // TODO: x2 Принцип DRY (don't repeat yourself, с буржуйского - неповторяйся)
-                    int b = in.nextInt();
-
-                    int[] array2 = new int[b];
-
-                    if (b > 20) break;
-
-                    System.out.println("Ввод >>> ");
-
-                    for (int i = 0; i < array2.length; i++) {
-                        array2[i] = in.nextInt();
-                    }
-
-                    for (int i : array2) {
+                    result = 1;
+                    for (double i : array) {
                         if (i % 5 != 0) result *= i;
                     }
                 break;
             }
-            // TODO: Почему ошибка? А если в массиве нету элементов?
-            if (result == 0) System.out.println("Error 147");
-                else System.out.println("Ответ >>> " + result);
+            // TODO: Почему ошибка? А если в массиве нет элементов?
+                System.out.println("Ответ >>> " + result);
     }
 }
